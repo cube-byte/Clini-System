6e1ae924871e39279a19fa890c31e2456ba51d7a
William Chipana <Williamchipana.com>
2026-04-18 15:26:19 -0500

    3 modelos agregados

---
+++ Backend/Clinic System/Clinic System/Models/Cita.cs
@@ -0,0 +1,15 @@
+﻿namespace Clinic_System.Models
+{
+    public class Cita
+    {
+        public int IdCitas { get; set; }
+        public DateTime FchCitas { get; set; }
+        public string Descripcion { get; set; }
+        public int IdPaciente { get; set; }
+        public string Estado { get; set; }
+
+        // Relación
+        public Paciente Paciente { get; set; }
+        public ICollection<Atencion> Atenciones { get; set; }
+    }
+}
